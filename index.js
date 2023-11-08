0de69b5b5f477e8e4bbef3e18c32e2471b5fe152
delvontedixon <delvontedixon@yahoo.com>
2023-11-08 12:11:12 -0500

    practice

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-console.log("Hello!");
-var bornAfter = 1980;
-var bornBefore = 1997;
-var birthYear = 1992;
+/* 
+Function: speakFriend
 
-if (bornBefore > birthYear && bornAfter < birthYear) {
-  console.log("You are a Millenial");
+Write a Function named speakFriend that takes one parameter.
+In the body of the Function, return the String "Access Denied"
+Except if the passed in value is equal to "Mellon", then return the String "Enter"
+Note: The Function has already been invoked for you, using the given variable a as the argument (see the last line of code)
+*/
+var myFruit = "Mellon";
+var myBasket = 123;
+
+// Step 1 write a function named speakFriend
+// Step 2 create a parameter in speakFriend function
+function speakFriend(sky) {
+  console.log(" running speakFriend function... ", sky);
+
+  // Step 3 If sky is equal to Mellon then return the string enter - otherwise return access denied
+  if (sky === "Mellon") {
+    console.log("Hello");
+    return sky + " Hello ";
+  } else if (myBasket === 123 && "Mellonhh") {
+    console.log("i have this much fruit", myBasket);
+  }
+  // if its not mellon then its something else
+  else {
+    console.log("Access");
+  }
 }
+
+// Calling speak friend method and passing in a variable
+console.log("calling funtion;", speakFriend(myFruit));

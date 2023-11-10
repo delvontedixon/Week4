c8a7e6072037471d78cdc125fef0cc523b1cf6f2
delvontedixon <delvontedixon@yahoo.com>
2023-11-09 20:55:19 -0500

    week5 class practice

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
-/* 
-Function: speakFriend
+// Declare a variable that stores your age
+// Declare a variable that stores your name
 
-Write a Function named speakFriend that takes one parameter.
-In the body of the Function, return the String "Access Denied"
-Except if the passed in value is equal to "Mellon", then return the String "Enter"
-Note: The Function has already been invoked for you, using the given variable a as the argument (see the last line of code)
-*/
-var myFruit = "Mellon";
-var myBasket = 123;
+// Write a conditional statement that will output
+// "You WON!" if your name contains an "a" or "A"
+// and your age is between 27 and 31 (inclusive).
+// If your age is less than 21, then output "NOT Eligible To play"
+// Otherwise output "Try AGAIN!".
 
-// Step 1 write a function named speakFriend
-// Step 2 create a parameter in speakFriend function
-function speakFriend(sky) {
-  console.log(" running speakFriend function... ", sky);
+// Output hello 5 times in a row
 
-  // Step 3 If sky is equal to Mellon then return the string enter - otherwise return access denied
-  if (sky === "Mellon") {
-    console.log("Hello");
-    return sky + " Hello ";
-  } else if (myBasket === 123 && "Mellonhh") {
-    console.log("i have this much fruit", myBasket);
-  }
-  // if its not mellon then its something else
-  else {
-    console.log("Access");
+// Using a traditional for loop, output
+// the even numbers from 1 - 20 (i.e. 2,4,6,...20)
+
+// Using a while loop, simulate a count down for
+// the start of an event (i.e. 10
+//                             9
+//                             ...
+//                             1
+//                             Start!
+//                        )
+
+// Advanced:
+// Using the loop of your choice, output
+// a classic multiplication table for the number 1 - 10
+// For example:
+//
+//   1  2  3  4  5  6  7  8  9  10
+//   2  4  5  8  10 12 14 16 18 20
+//   3  6  9  12 15 18 21 24 27 30
+//   ...
+//   10 20 30 40 50 60 70 80 90 100
+
+for (let y = 1; y <= 10; y++) {
+  for (let x = 1; x <= 10; x++) {
+    console.log(x * y);
   }
 }
-
-// Calling speak friend method and passing in a variable
-console.log("calling funtion;", speakFriend(myFruit));
